b9ec88d6710d2f7673219256fcea3b6ed2d76c04
Jorge Pérez <77463563+per18364@users.noreply.github.com>
2024-02-15 20:15:38 -0600

    SIIIIUUUU

--- src/app/auth/auth-config.module.ts
+++ src/app/auth/auth-config.module.ts
@@ -5,15 +5,15 @@ import { AuthModule } from 'angular-auth-oidc-client';
   imports: [
     AuthModule.forRoot({
       config: {
-        // authority: 'https://login.microsoftonline.com/common/v2.0',
-        authority:
-          'https://login.microsoftonline.com/f8cdef31-a31e-4b4a-93e4-5f571e91255a/v2.0',
+        authority: 'https://login.microsoftonline.com/common/v2.0',
+        // authority:
+        //   'https://login.microsoftonline.com/f8cdef31-a31e-4b4a-93e4-5f571e91255a/v2.0',
         // authWellknownEndpointUrl:
         //   'https://login.microsoftonline.com/common/v2.0',
         // redirectUrl: window.location.origin,
         redirectUrl: 'http://localhost:4200/profile',
-        postLogoutRedirectUri: 'http://localhost:4200/home',
-        clientId: 'd50a85fa-cf60-4969-b0bf-c6066b2d4201',
+        // postLogoutRedirectUri: 'http://localhost:4200/home',
+        clientId: '2526451a-be65-4805-a711-5df255a39832', //V3
         scope: 'offline_access openid profile User.Read', // 'openid profile offline_access ' + your scopes
         responseType: 'code',
         silentRenew: true,
